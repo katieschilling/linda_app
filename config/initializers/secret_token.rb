e80c630c55f53bf01c599bab75ce3b7177efc890
Princess Katie <kdschilling@gmail.com>
2014-09-13 10:39:56 -0500

    Update Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LindaApp::Application.config.secret_key_base = '18b79011ec65e2593bfd3ac59d2a5e216046b2b10bc6cf4fbd28e29a2371a15caf81d42c67edaec55bdad1d529f403d8e0215556020f28547d54a422610b1f36'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LindaApp::Application.config.session_store :cookie_store, key: '_linda_app_session'
